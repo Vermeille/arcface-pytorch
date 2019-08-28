b967e125a3c6a3bd0b49cc00dbd6f9db52b0eb15
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-08-28 10:13:58 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+        name='arface-pytorch',
+        version='0.1dev',
+        packages=['face_rec',],
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
